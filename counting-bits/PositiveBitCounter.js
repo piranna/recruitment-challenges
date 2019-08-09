2fa123f3cec3118183e9653610102fc14b02a594
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2019-08-09 17:30:33 +0200

    Implement solution for `counting-bits` problem

--- counting-bits/PositiveBitCounter.js
+++ counting-bits/PositiveBitCounter.js
@@ -1,5 +1,21 @@
 function Count (input) {
-  throw new Error('Not implemented')
+  if(!Number.isInteger(input)) throw SyntaxError('`input` must be an Integer')
+  if(input < 0) throw RangeError('Negative numbers are not supported')
+
+  const result = new Array(1)
+
+  let total = 0
+
+  for(let index = 0; input; index++, input >>= 1)
+    if(input & 1)
+    {
+      total++
+      result.push(index)
+    }
+
+  result[0] = total
+
+  return result
 }
 
 module.exports = { Count }
