d5643c47143b55cb30f0e7f31243e77e40071a64
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2019-08-09 17:32:00 +0200

    [counting-bits] Fixed linting

--- counting-bits/PositiveBitCounter.js
+++ counting-bits/PositiveBitCounter.js
@@ -1,17 +1,17 @@
 function Count (input) {
-  if(!Number.isInteger(input)) throw SyntaxError('`input` must be an Integer')
-  if(input < 0) throw RangeError('Negative numbers are not supported')
+  if (!Number.isInteger(input)) throw SyntaxError('`input` must be an Integer')
+  if (input < 0) throw RangeError('Negative numbers are not supported')
 
   const result = new Array(1)
 
   let total = 0
 
-  for(let index = 0; input; index++, input >>= 1)
-    if(input & 1)
-    {
+  for (let index = 0; input; index++, input >>= 1) {
+    if (input & 1) {
       total++
       result.push(index)
     }
+  }
 
   result[0] = total
 
